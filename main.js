a1b05115791b16edad2bf832827346974359174f
Thiago Bittencourt Coelho <62147217+bittencourthiago@users.noreply.github.com>
2020-07-22 01:32:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function bibleBot() {
     const about = { version: '1.0', type: 'Nova Versão Internacional' }
     const Discord = require("discord.js");
 
+
     //criando bot
     const bot = new Discord.Client();
 
@@ -11,7 +12,6 @@ function bibleBot() {
 
     bot.login('NzI1OTE3MTM0NjM4ODA5MDkw.XvVtgw.7GmuDrxb6iM7TX-wLQ5UH8G95xg');
 
-<<<<<<< HEAD
     bot.once('ready', () => {
         console.log(`Bot online: ${bot.user.tag}!`);
     });
@@ -23,15 +23,6 @@ function bibleBot() {
 
     //monitorando mensagens
     bot.on('message', msg => {
-=======
-//mensagem de boas vindas
-bot.on('guildMemberAdd', membro => {
-    membro.send(`Seja bem-vindo(a)`);
-});
-
-//monitorando mensagens
-bot.on('message', msg => {
->>>>>>> b6dff4821174be1e568ef8679ed7d2ebc90a5027
 
         indexChar = (char) => msg.content.indexOf(char) >= 0;
 
@@ -50,10 +41,6 @@ bot.on('message', msg => {
         }
     });
 
-<<<<<<< HEAD
 }
 
 bibleBot()
-=======
-});
->>>>>>> b6dff4821174be1e568ef8679ed7d2ebc90a5027
